53f0b72dce930d76335369b0994848dbf6688a7d
Masquerade51256 <978301453@qq.com>
2018-11-22 19:35:11 +0800

    Project Building

---
+++ app/src/main/java/com/rpgroup/repic/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.rpgroup.repic
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
